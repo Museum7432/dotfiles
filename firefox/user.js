45b8838117b77eddeee286d25c3978b99fa123fe
21522188 <21522188@gm.uit.edu.vn>
2024-04-08 21:15:16 +0700

    minor changes

--- firefox/user.js
+++ firefox/user.js
@@ -33,7 +33,7 @@ user_pref("privacy.firstparty.isolate", true);
 user_pref("privacy.resistFingerprinting", true);
 // change user agent
 // user_pref("general.useragent.override", "Mozilla/5.0 (Windows NT 10.0; rv:122.0) Gecko/20100101 Firefox/122.0");
-user_pref("general.platform.override", "Win32");
+// user_pref("general.platform.override", "Win32");
 
 user_pref("toolkit.telemetry.enabled", false);
 user_pref("webgl.disabled", true);
@@ -86,32 +86,13 @@ user_pref("nglayout.initialpaint.delay_in_oopif", 0); // default=5
 // [3] https://www.troddit.com/r/firefox/comments/tbphok/is_setting_gfxwebrenderprecacheshaders_to_true/i0bxs2r/
 // [4] https://www.troddit.com/r/firefox/comments/z5auzi/comment/ixw65gb?context=3
 user_pref("gfx.webrender.all", true); // enables WR (GPU) + additional features
-user_pref("gfx.webrender.precache-shaders", true);
-user_pref("gfx.webrender.compositor", true);
-user_pref("gfx.webrender.compositor.force-enabled", true); // enforce
-user_pref("layers.gpu-process.enabled", true);
-user_pref("layers.gpu-process.force-enabled", true); // enforce
-user_pref("media.hardware-video-decoding.enabled", true);
-user_pref("media.hardware-video-decoding.force-enabled", true); // enforce
-
-// PREF: GPU-accelerated Canvas2D
-// [WARNING] May break PDF rendering on Surface Pro devices [2]
-// [1] https://bugzilla.mozilla.org/show_bug.cgi?id=1739448
-// [2] https://github.com/yokoffing/Betterfox/issues/153
-user_pref("gfx.canvas.accelerated", true); // DEFAULT on macOS and Linux v.110
-user_pref("gfx.canvas.accelerated.cache-items", 32768);
-user_pref("gfx.canvas.accelerated.cache-size", 4096);
-user_pref("gfx.content.skia-font-cache-size", 80);
-
-// PREF: image tweaks
-user_pref("image.cache.size", 10485760);
-user_pref("image.mem.decode_bytes_at_a_time", 131072); // alt=65536; preferred=262144; chunk size for calls to the image decoders
-user_pref("image.mem.shared.unmap.min_expiration_ms", 120000); // default=60000; minimum timeout to unmap shared surfaces since they have been last used
-
-// PREF: increase media cache
-user_pref("media.memory_cache_max_size", 1048576); // alt=512000; also in Securefox (inactive there)
-user_pref("media.memory_caches_combined_limit_kb", 2560000); // preferred=3145728; // default=524288
-//user_pref("media.memory_caches_combined_limit_pc_sysmem", 20); // default=5
+// user_pref("gfx.webrender.precache-shaders", true);
+// user_pref("gfx.webrender.compositor", true);
+// user_pref("gfx.webrender.compositor.force-enabled", true); // enforce
+// user_pref("layers.gpu-process.enabled", true);
+// user_pref("layers.gpu-process.force-enabled", true); // enforce
+// user_pref("media.hardware-video-decoding.enabled", true);
+// user_pref("media.hardware-video-decoding.force-enabled", true); // enforce
 
 // PREF: disable AV1 for hardware decodeable videos
 // AV1 uses software (CPU-based) decoding
@@ -126,4 +107,4 @@ user_pref("media.av1.enabled", false);
 // PREF: increase memory cache size
 // [1] https://www.makeuseof.com/tag/how-much-data-does-youtube-use/
 //user_pref("browser.cache.memory.capacity", -1); // DEFAULT; 256000=256MB, 512000=512MB, 1024000=1GB, 2097152=2GB, 5242880=5GB, 8388608=8GB
-user_pref("browser.cache.memory.max_entry_size", 512000); // alt=51200; preferred=327680 ; alt= -1 -> entries bigger than than 90% of the mem-cache are never cached
+user_pref("browser.cache.memory.max_entry_size", -1); // alt=51200; preferred=327680 ; alt= -1 -> entries bigger than than 90% of the mem-cache are never cached
